dbe19ba8174b1daf33b990acc6647a99e3d3243c
GanzYe <bahadirov4@gmail.com>
2020-11-10 02:19:06 +0600

    add rc content

--- src/Pages/RCPage/components/content/content.js
+++ src/Pages/RCPage/components/content/content.js
@@ -8,10 +8,16 @@ export default class Content extends Component {
         return (
             <div className="content_container">
                     <div className="block_container">
-                        <RCS height={200} clipPath ="polygon(18% 0, 43% 6%, 38% 19%, 26% 16%, 12% 64%, 22% 85%, 76% 72%, 79% 84%, 11% 100%, 0 63%)"/>
-                        <RCS height={200} clipPath="polygon(25% 0%, 49% 0, 34% 50%, 52% 100%, 25% 100%, 9% 51%)"/>
-                        <RCS height={200} clipPath="polygon(25% 0%, 49% 0, 34% 50%, 52% 100%, 25% 100%, 9% 51%)"/>
-                        <RCS height={170} clipPath="polygon(25% 0%, 49% 0, 34% 50%, 52% 100%, 25% 100%, 9% 51%)"/>
+                        <div className="block_rc_container">
+                            <RCS height={180} width="45%" clipPath ="polygon(18% 0, 43% 6%, 38% 19%, 26% 16%, 12% 64%, 22%     85%, 76% 72%, 79% 84%, 11% 100%, 0 63%)"/>
+                            <RCS height={120} width="15%" clipPath="polygon(25% 0%, 75% 0%, 45% 40%, 75% 100%, 25% 100%, 0 40%)"/>
+                            <RCS height={120} width="15%" clipPath="polygon(25% 0%, 75% 0%, 41% 45%, 75% 100%, 25% 100%, 0 50%)"/>
+                            <RCS height={110} width="25%" clipPath="polygon(25% 0%, 49% 0, 34% 50%, 52% 100%, 25% 100%, 9%     51%)"/>
+                            <RCS height={120} width="17%" clipPath="polygon(12% 10%, 60% 0, 83% 90%, 33% 100%)"/>
+                            <RCS height={120} width="17%" clipPath="polygon(12% 10%, 60% 0, 83% 90%, 33% 100%)"/>
+                            <RCS height={120} width="17%" clipPath="polygon(12% 10%, 60% 0, 83% 90%, 33% 100%)"/>
+                            <RCS height={170} width="40%" clipPath="polygon(0% 15%, 20% 10%, 30% 76%, 66% 78%, 83% 15%, 72%     13%, 76% 1%, 100% 6%, 80% 100%, 21% 95%, 7% 80%)"/>
+                        </div>
                     </div>
             </div>
         );
--- src/Pages/RCPage/components/content/rcs/rcs.js
+++ src/Pages/RCPage/components/content/rcs/rcs.js
@@ -6,16 +6,21 @@ export default class RCS extends Component {
 
     render() {
         const {height} = this.props;
+        const {width} = this.props;
         const {clipPath} = this.props;
         console.log(height);
         return (
-            <div className="res_block" style={{
+            <div className="res_block" onClick={
+                ()=>{
+                    
+                }
+            } style={{
                 display: "block",
-                width: "90%",
+                width: width,
                 height: height,
-                "background-color": "#E7E7E8",
-                "box-shadow":  "5px 5px 10px #a3a3a3, -5px -5px 10px linear-gradient(90deg, rgba(0,83,147,1) 0%, rgba(0,113,185,1) 100%)",
-                "clip-path":clipPath
+                "backgroundColor": "white",
+                "boxShadow":  "5px 5px 10px #a3a3a3, -5px -5px 10px linear-gradient(90deg, rgba(0,83,147,1) 0%, rgba(0,113,185,1) 100%)",
+                "clipPath":clipPath
             }}></div>
             
         );
