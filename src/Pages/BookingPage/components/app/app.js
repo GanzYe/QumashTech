3f3c1f6351a3f1b8092b93065f9e6c41115fa590
GanzYe <bahadirov4@gmail.com>
2020-11-10 04:06:14 +0600

    new booking

--- src/Pages/BookingPage/components/app/app.js
+++ src/Pages/BookingPage/components/app/app.js
@@ -4,14 +4,14 @@ export default class App extends Component {
     render(){
       
     return (
-        <div className="bookingPage" >
+        <div className="bookingPage container" >
             <h1>Бронирование</h1>
-                <input type={Text} disabled={false}     placeholder="Имя *"/>
+                <input type={Text} disabled={false} className="form-control"     placeholder="Имя *"/>
             
-                <input type={Text} disabled={false}placeholder="Фамилия *"/>
+                <input type={Text} disabled={false} className="form-control" placeholder="Фамилия *"/>
             
             
-                <input type={Text} disabled={false}placeholder="Электронная почта *"/>
+                <input type={Text} disabled={false}placeholder="Электронная почта *" className="form-control"/>
             
             
                 <input disabled={false} type={Text}placeholder="Номер телефона *"/>
