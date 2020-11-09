3e7eaf6168dc4b020f95b2142b1293282226d0c2
GanzYe <bahadirov4@gmail.com>
2020-11-10 02:15:56 +0600

    add some features to blockpage

--- src/Pages/BlockPage/components/block/block.js
+++ src/Pages/BlockPage/components/block/block.js
@@ -5,51 +5,18 @@ import './block.css'
 
 export default class Block extends Component {
 
-
     render() {
+/*     const {polygonParams} = this.props; */
+       const polygonParams = "polygon(25% 0%, 49% 0, 34% 50%, 52% 100%, 25% 100%, 9% 51%)"
         return (
-            <div className="content_container" style={{
-                width: '100%',
-                height: '100%',
-                display: 'flex',
-                justifyContent: 'center'
-            }}>
-                    <div className="block_container" style ={{
-                           display: 'flex',
-                           justifyContent: 'center',
-                           alignItems: 'center',
-                           width: '90%',
-                           height: '70vh',
-                           borderRadius: '10px',
-                           background: 'linear-gradient(90deg, rgba(0,83,147,1) 0%, rgba(0,113,185,1) 100%)',
-                           boxShadow:  '5px 5px 10px #a3a3a3, -5px -5px 10px #ffffff'
-                    }}>
+            <div className="content_container">
+                    <div className="block_container">
                         <div className="res_block" style={{
-                                display: 'block',
-                                width: '90%',
-                                height: '80%',
-                                'backgroundColor': '#E7E7E8',
-                                'boxShadow':  "5px 5px 10px #a3a3a3, -5px -5px 10px linear-gradient(90deg, rgba(0,83,147,1) 0%, rgba(0,113,185,1) 100%)",
-                                "clipPath": `polygon(${25}% 0%, 49% 0, 34% 50%, 52% 100%, 25% 100%, 9% 51%)`
+                                "clipPath": polygonParams
                         }}>
-                        <div className="coworking_block" onClick={()=>console.log('test')} style={{
-                                display: 'block',
-                                width: '95px',
-                                height: '100px',
-                                'clipPath': 'polygon(79% 0, 100% 0, 88% 100%, 57% 100%)',
-                                position: 'absolute',
-                                'backgroundColor': '#ACA9A9'
-                        }}></div>
+                        <div className="coworking_block" onClick={()=>console.log('test')}><i className="fas fa-chair"></i></div>
                         </div>
-                        <div className="sport_block" onClick={()=>console.log('test')} style={{
-                                display: 'block',
-                                position: 'absolute',
-                                left: '200px',
-                                background: '#ACA9A9',
-                                width: '20%',
-                                height: '20%',
-                                borderRadius: '5px'
-                        }}></div>
+                        <div className="sport_block" onClick={()=>console.log('test')}><i className="fas fa-futbol"></i></div>
                     </div>
                 </div>
      
