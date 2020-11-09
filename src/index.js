7d44621689b507ac2c88ad9e41f4cbc3ebd1bb39
GanzYe <bahadirov4@gmail.com>
2020-11-10 03:13:23 +0600

    index tested

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
-import App from './Pages/BlockPage/components/app';
+/* import App from './Pages/BlockPage/components/app'; */
+import App from './Pages/BookingPage/components/app';
 
 
 ReactDOM.render(<App/>,document.getElementById('root'));
