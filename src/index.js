f57ebdaeb3766768b95222076d92fa0c17bc5cd6
GanzYe <bahadirov4@gmail.com>
2020-11-10 18:01:35 +0600

    +

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './Pages/MainPage/components/app';
 import RcPage from './Pages/RCPage/components/app';
 import BlockApp from './Pages/BlockPage/components/app';
@@ -20,15 +19,4 @@ function AppMain(){
     )
 }
 ReactDOM.render(<AppMain/>,document.getElementById('root'));
-=======
-<<<<<<< HEAD
-import App from './Pages/CoworkingPage/components/app';
-/* import App from './Pages/BookingPage/components/app'; */
-/* import App from './Pages/SportPage/components'; */
-=======
-import App from './Pages/BookingPage/components/app';
->>>>>>> 1319d78d64b4bb0cde05bb3b6ea0b38f9bfbf0e7
 
-
-ReactDOM.render(<App/>,document.getElementById('root'));    
->>>>>>> 9c14bc9371491194bb291b0ae4bf877bf3937ee7
