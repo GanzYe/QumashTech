aaf336881e376ef12c9cd76c48513f71af5b265b
unknown <erabritania@gmail.com>
2020-11-10 13:33:54 +0600

    save sport and booking

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import App from './Pages/MainPage/components/app';
+import App from './Pages/BookingPage/components/app';
 
 
 ReactDOM.render(<App/>,document.getElementById('root'));
