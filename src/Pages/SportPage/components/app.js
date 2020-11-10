60e24b9750d6ec20bf7231874c1229e40b283dfc
muccurly <mirmiras04@mail.ru>
2020-11-10 20:13:48 +0600

    save erasyk code

--- src/Pages/SportPage/components/app.js
+++ src/Pages/SportPage/components/app.js
@@ -1,10 +1,10 @@
 import React,{Component} from 'react';
 import './app.css';
-import img from './footbal.png'
-import img2 from './basketbal.png'
-import img3 from './R.png'
-import img4 from './L.png'
-import imgBack from './ArrL.png'
+import img from './imgs/footbal.png'
+import img2 from './imgs/basketbal.png'
+import img3 from './imgs/R.png'
+import img4 from './imgs/L.png'
+import imgBack from './imgs/ArrL.png'
 export default class App extends Component {
     render(){
     
@@ -23,42 +23,42 @@ export default class App extends Component {
         </div>
       <div className='SportPage '>
             
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div id="carouselExampleInterval" class="carousel slide" data-interval="false" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src={img} class="d-block w-100" alt="..."/>
     </div>
-    <div class="carousel-item">
+    <div class="carousel-item" >
       <img src={img2} class="d-block w-100" alt="..."/>
     </div>
-    
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>    
+</div>   
         
   </div>
 <div className='CheckBoxx'>
 <input  type="checkbox" id="agree"/>
-            <label class="remember" for="agree">Хватает ли игроков?</label>
+            <label class="remember" for="agree">Не хватает ли игроков?</label>
 </div>
-<div className='Comman'>
-  <div className='contp'>
-    <p className='players'>Количество игроков</p>
-    <input className='Sshet' type={Number}/>
+ <div className='Comman'>
+   <div className='contp'>
+     <p className='players'>Количество игроков</p>
+     <input className='Sshet' type={Number}/>
+   </div>
+   <div className='contp'>
+     <p className='players'>Количество нужных игроков</p>
+     <input  className='Sshet' type={Number}/>
+   </div>
+   <button className='playersbtn'>Таблица команд</button>
   </div>
-  <div className='contp'>
-    <p className='players'>Количество нужных игроков</p>
-    <input  className='Sshet' type={Number}/>
-  </div>
-  <button className='playersbtn'>Таблица команд</button>
-  </div>
+   <button className='btnBooking'>Забронировать</button>
     </div>
 
 
