93a170e0f34964c21f0804d1657d02c82cdf91f5
unknown <erabritania@gmail.com>
2020-11-11 00:39:56 +0600

    FINAL SAVE ERASYL

--- src/Pages/SportPage/components/app.js
+++ src/Pages/SportPage/components/app.js
@@ -11,21 +11,18 @@ export default class App extends Component {
  
    if(id==1 ){
     console.log(id)
-    let mas= document.querySelectorAll('.Sshet');
-    
-    mas.forEach(element => {
-      console.log(element);
-      element.disabled = true;
-    });
+   document.querySelector('.Sshet').disabled = true;
+    document.querySelector('.Sshet2').disabled = true;
+      
     document.querySelector('.playersbtn').disabled = true;
+    document.querySelector('.Comman').style.display='none';
     }
     else{
       console.log(id)
-    let mas= document.querySelectorAll('.Sshet');
-    mas.forEach(element => {
-      element.disabled = false;
-    });
+      document.querySelector('.Sshet').disabled = false;
+      document.querySelector('.Sshet2').disabled = false;
     document.querySelector('.playersbtn').disabled = false;
+    document.querySelector('.Comman').style.display='block';
     }}
 render(){
 
@@ -34,8 +31,6 @@ render(){
       
       <div className='Sportera d-flex flex-column justify-content-center'>
           <img src={imgBack} class="BackImg" alt="..."/>
-          <img src={img3} class="StrelkaR" alt="..."/>
-          <img src={img4} class="StrelkaL" alt="..."/>
         <div>
         </div>
         <div className='SportText'>
@@ -44,16 +39,19 @@ render(){
           </label>
         </div>
       <div className='SportPage '>
-            
+     
       <div id="carouselExampleInterval" class="carousel slide" data-interval="false" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active">
+    <div class="carousel-item active Mid">
       <img src={img} class="d-block w-100" alt="..."/>
+      <p className='PforImg'>Footbal</p>
     </div>
-    <div class="carousel-item" >
+    <div class="carousel-item Mid" >
       <img src={img2} class="d-block w-100" alt="..."/>
+      <p className='PforImg'>Basketbal</p>
     </div>
   </div>
+  <div className='Arrows'>
   <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -62,19 +60,17 @@ render(){
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>   
+</div>   </div>
         
   </div>
-<div className='CheckBoxx'>
-<input class="magic-radio" type="radio" name="radio" id="1" value="option"onClick={()=> {this.Check(1)}}/>
-<label for="1">
-  Хватает игроков
-</label>  
-<input class="magic-radio" type="radio" name="radio" id="2" value="option" onClick={()=> {this.Check(2)}}/>
-<label for="2">
-  Нехватает игроков
-</label> 
-</div>
+        <h1 className='question'>Хватает ли игроков?</h1>
+  <div className='container'>
+        <input type='radio' name='size' id='small' onClick={()=> {this.Check(1)}}/>
+        <label className='RadioL' for='small' aria-checked='true' >Да</label><br></br>
+        <input  type='radio' name='size'defaultChecked  id='large'/>
+        <label className='RadioL' for='large'  onClick={()=> {this.Check(2)}}>Нет</label>
+        
+      </div>
  <div className='Comman' aria-disabled='true'>
    <div className='contp'>
      <p className='players'>Количество игроков</p>
@@ -82,7 +78,7 @@ render(){
    </div>
    <div className='contp'>
      <p className='players'>Количество нужных игроков</p>
-     <input  className='Sshet' type={Number}/>
+     <input  className='Sshet2' type={Number}/>
    </div>
    <button className='playersbtn'>Таблица команд</button>
   </div>
